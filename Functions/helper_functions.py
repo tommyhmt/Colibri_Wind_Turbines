9cd44ad47606eb85519795eaf6f18aeba8513a37
tommyhmt <112706311+tommyhmt@users.noreply.github.com>
2026-06-21 14:45:22 +0100

    Update helper_functions.py

--- Functions/helper_functions.py
+++ Functions/helper_functions.py
@@ -1,17 +1,17 @@
-# import libraries
-from pyspark.sql.functions import when, col, coalesce, raise_error, lit
-from pyspark.sql import DataFrame
-from pyspark.sql.types import StructType
+from pyspark.sql.functions import current_timestamp, col
+from pyspark.sql import DataFrame, SparkSession
+spark = SparkSession.builder.getOrCreate()
 
-def apply_schema(df: DataFrame, schema: StructType, fillnull: bool = False) -> DataFrame:
+def read_csv(path) -> DataFrame:
 
-    schema_columns = [(when(col(x.name).isNotNull(), coalesce(col(x.name).cast(x.dataType), raise_error(f"Cannot cast column '{x.name}' to '{x.dataType}'"))).otherwise(col(x.name).cast(x.dataType))).alias(x.name) if x.name.upper() in (c.upper() for c in df.columns) else lit(None).cast(x.dataType).alias(x.name) for x in schema.fields]
-
-    df = df.select(*schema_columns)
-
-    # Fill Nulls
-    if fillnull == True:
-        df = df.na.fill("UNDEF",[x[0] for x in df.dtypes if x[1] == "string"])
-        df = df.na.fill(0,[x[0] for x in df.dtypes if ("decimal" in x[1]) or (x[1] in ("double", "float", "int", "bigint", "smallint"))])
+    df = (
+                spark.readStream.format("cloudFiles")
+                .option("cloudFiles.format", "csv")
+                .option("delimiter", ",")
+                .option("inferSchema", False)
+                .load(f"/mnt/{path}/")
+                .withColumn("input_file_name", col("_metadata.file_path"))
+                .withColumn("import_timestamp", current_timestamp())
+                )
 
     return df
